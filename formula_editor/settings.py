225c82f53f402d7b19594cc001e1945389a41fe4
Kirill <verikrill@mail.ru>
2024-12-15 23:23:43 +0300

    commit

--- formula_editor/settings.py
+++ formula_editor/settings.py
@@ -110,7 +110,7 @@ LOGIN_URL = '/login/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
